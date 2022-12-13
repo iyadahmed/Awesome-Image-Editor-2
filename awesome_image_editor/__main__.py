84e906444f2062ba57a1c60009b7e5567f12f1c0
Iyad Ahmed <iyadahmed430@gmail.com>
2022-12-13 23:40:11 +0200

    Use a start.py file to launch application

--- awesome_image_editor/__main__.py
+++ awesome_image_editor/__main__.py
@@ -3,7 +3,13 @@ import sys
 from .app import Application
 from .mainwindow import MainWindow
 
-app = Application(sys.argv)
-main_window = MainWindow()
-exit_code = app.exec()
-sys.exit(exit_code)
+
+def main():
+    app = Application(sys.argv)
+    main_window = MainWindow()
+    exit_code = app.exec()
+    sys.exit(exit_code)
+
+
+if __name__ == "__main__":
+    main()
---
+++ start.py
@@ -0,0 +1,3 @@
+from awesome_image_editor import __main__
+
+__main__.main()
