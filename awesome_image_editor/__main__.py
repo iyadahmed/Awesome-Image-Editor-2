9a5e08f7c7cb4ea012755faa3bbc7109015a9a1b
Iyad Ahmed <iyadahmed430@gmail.com>
2022-12-31 16:24:00 +0200

    Force use of X11 Qt plugin on Linux to avoid issues with moving QDockWidget and QToolBar on Wayland

--- awesome_image_editor/__main__.py
+++ awesome_image_editor/__main__.py
@@ -1,4 +1,11 @@
 import sys
+import os
+import platform
+
+# Force use of X11 Qt plugin on Linux to avoid issues with moving QDockWidget and QToolBar on Wayland
+# https://github.com/KDAB/KDDockWidgets/issues/10
+if platform.system() == "Linux":
+    os.environ["QT_QPA_PLATFORM"] = "xcb"
 
 from .app import Application
 from .mainwindow import MainWindow
